dad69053eced878ce4f582b7e5bdfabdf203ddf6
Erica Arenas <erica.arenas.rodriguez@gmail.com>
2022-09-11 13:09:31 +0200

    directivas de atributo y estructurales

--- HolaMundo/src/app/modules/lists/basic-list/basic-list.component.ts
+++ HolaMundo/src/app/modules/lists/basic-list/basic-list.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+
+export type Producto = {
+  nombre: string;
+  precio: number;
+  descripcion: string;
+}
+
 @Component({
   selector: 'app-basic-list',
   templateUrl: './basic-list.component.html',
@@ -7,9 +14,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicListComponent implements OnInit {
 
+  listaElementos: Producto[] = [
+    {
+      nombre: 'Leche',
+      precio: 10,
+      descripcion: 'Leche entera'
+    },
+    {
+      nombre: 'Carne',
+      precio: 20,
+      descripcion: 'Ternera'
+    }
+  ];
+  cargando: boolean = true;
+  opcion: number = 0;
+
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
+  cambiarCargando(): void {
+    this.cargando = !this.cargando;
+  }
+
+  escogerOpcion(opcionEscogida: number) {
+    this.opcion = opcionEscogida; //el valor cambia, implica un cambio en los elementos renderizados
+  }
 }
