cc87dd8319ba3f1785916252077fdae49862beed
Erica Arenas <erica.arenas.rodriguez@gmail.com>
2022-09-15 11:45:20 +0200

    Observables and subscriptions

--- HolaMundo/src/app/components/lista-contactos/lista-contactos.component.ts
+++ HolaMundo/src/app/components/lista-contactos/lista-contactos.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ContactoService } from '../../services/contacto.service';
 import { IContacto } from '../../models/contacto.interface';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-lista-contactos',
   templateUrl: './lista-contactos.component.html',
   styleUrls: ['./lista-contactos.component.scss']
 })
-export class ListaContactosComponent implements OnInit {
+export class ListaContactosComponent implements OnInit, OnDestroy {
 
   listaContactos: IContacto[] = [];
   contactoSeleccionado: IContacto | undefined;
+
+  //susbscripción del servicio
+  subscripcion: Subscription | undefined;
   //se inyecta el servicio a nivel raiz, para este componente
   constructor(private contactoService: ContactoService) { }
 
@@ -25,10 +29,15 @@ export class ListaContactosComponent implements OnInit {
 
   obtenerContacto (id: number) {
     // console.log(`Obtener información del contacto con ID: ${id}`)
-    this.contactoService.obtenerContactoPorID(id)
-    ?.then((contacto: IContacto) => this.contactoSeleccionado = contacto)
-    .catch((error) => console.error(`Ha habido un error al recuperar los detalles del contacto: ${error}`))
-    .finally(() => console.log('Petición de contacto por id terminada'));
+    // this.contactoService.obtenerContactoPorID(id)
+    // ?.then((contacto: IContacto) => this.contactoSeleccionado = contacto)
+    // .catch((error) => console.error(`Ha habido un error al recuperar los detalles del contacto: ${error}`))
+    // .finally(() => console.log('Petición de contacto por id terminada'));
+    this.subscripcion = this.contactoService.obtenerContactoPorID(id)?.subscribe(
+      (contacto:IContacto) => this.contactoSeleccionado = contacto)
   }
 
+  ngOnDestroy(): void {
+    this.subscripcion?.unsubscribe();
+  }
 }
--- HolaMundo/src/app/services/contacto.service.ts
+++ HolaMundo/src/app/services/contacto.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { CONTACTOS } from '../mocks/contactos.mock';
 import { IContacto } from '../models/contacto.interface';
 
+//importamos observable de rxjs
+import { Observable } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -13,11 +16,18 @@ export class ContactoService {
     // return CONTACTOS;
     return Promise.resolve(CONTACTOS);
   }
-  obtenerContactoPorID (id: number): Promise<IContacto> | undefined {
+  obtenerContactoPorID (id: number): Observable<IContacto> | undefined {
+    //creamos un observable
+    let observable: Observable<IContacto> = new Observable(observer => {
+      observer.next(contacto); //emitir un valor a todo componente suscrito
+      observer.complete(); //no emitimos más valores
+    })
+
     const contacto = CONTACTOS.find((contacto:IContacto) => contacto.id === id);
-    if(contacto)
-      {return Promise.resolve(contacto);}
-      else
-      {return;}
+    if(contacto) {
+      return observable;
+    } else {
+      return;
+    }
   }
 }
