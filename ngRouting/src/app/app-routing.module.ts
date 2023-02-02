b5218e2a7b61e5980ae1b0028584f91a540408f4
Erica Arenas <erica.arenas.rodriguez@gmail.com>
2023-02-02 19:49:13 +0100

    Adding routes and components

--- ngRouting/src/app/app-routing.module.ts
+++ ngRouting/src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactDetailPageComponent } from './pages/contact-detail-page/contact-detail-page.component';
+import { ContactsPageComponent } from './pages/contacts-page/contacts-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: 'home',
+    component: HomePageComponent
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent
+  },
+  {
+    path: 'contacts',
+    component: ContactsPageComponent
+  },
+  {
+    path: 'contacts/:id',
+    component: ContactDetailPageComponent
+  },
+  {
+    path: '**',
+    component: NotFoundPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
